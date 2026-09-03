cfbfad95470f2ec4044780cd79c069eedfdebbf4
shaliniselvaraj7 <shaliniselvaraj57@gmail.com>
2026-09-03 09:44:03 +0530

    Add files via upload

---
+++ TightCoupling/TightCoupling/src/test/java/com/example/TightCoupling/TightCouplingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.TightCoupling;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TightCouplingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
